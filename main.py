e6e9f813a5295173ae132433f6fb35a4f1c08f2e
MyBad123 <genag4448@gmail.com>
2023-06-06 15:21:06 +0300

    06.06.2023: random algorithm v1(beta with errors)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 import selenium.webdriver.remote.webelement
 from selenium import webdriver
@@ -59,25 +60,12 @@ class Browser:
         back_tab.click()
 
 
-# driver = webdriver.Chrome()
-# driver.get("https://yandex.ru/maps/193/voronezh/?ll=39.198713%2C51.633190&z=16.06")
-
-
 def test_write_plus_it(browser_class: Browser):
     time.sleep(3)
     text_box_input = browser_class.driver.find_element(by=By.CSS_SELECTOR, value="input")
+    for i in range(20):
+        text_box_input.send_keys("\ue003")
     text_box_input.send_keys("Плюс Ай Ти" + Keys.ENTER)
-
-
-# test_write_plus_it()
-
-
-
-
-# def go_photo():
-    """get photo from card"""
-
-#    return get_element_from_carousel('Фото и видео')
 
 
 # get photos
@@ -152,6 +140,7 @@ class YandexReviews:
         self.browser.driver.execute_script("document.querySelector('.scroll__container').\
                     scrollTo(0, document.querySelector('.scroll__container').scrollHeight)")
 
+
 class YandexAuth:
 
     def __init__(self, browser: Browser):
@@ -169,12 +158,14 @@ class YandexAuth:
     def __get_enter_btn(self):
         """get enter btn from burger menu"""
 
-        time.sleep(2)
+        time.sleep(5)
 
         return self.browser.driver.find_element(by=By.LINK_TEXT, value='Войти')
 
     def __get_input_by_css(self, css_selector):
         """search input by css selector"""
+
+        time.sleep(2)
 
         return self.browser.driver.find_element(
             by=By.CSS_SELECTOR, value=css_selector
@@ -197,7 +188,7 @@ class YandexAuth:
             .find_element(by=By.CSS_SELECTOR, value='.passp-sign-in-button') \
             .find_element(by=By.CSS_SELECTOR, value='button')
 
-    def auth(self, login, password):
+    def auth(self, login, password, return_func):
         """use all methods for auth in yandex maps"""
 
         self.__get_menu_btn().click()
@@ -210,12 +201,14 @@ class YandexAuth:
         self.__input_text(text=password, selector='input[type="password"]')
         self.__get_push_login_btn().click()
 
-
-#photo_elem = go_photo()
-#photo_elem.click()
+        return_func(self.browser)
 
 
-#def scroll_content():
+# photo_elem = go_photo()
+# photo_elem.click()
+
+
+# def scroll_content():
     """scroll all photo of company"""
 
 #    time.sleep(2)
@@ -223,10 +216,10 @@ class YandexAuth:
 #            scrollTo(0, document.querySelector('.scroll__container').scrollHeight)")
 
 
-#scroll_content()
+# scroll_content()
 
 
-#def go_reviews():
+# def go_reviews():
     """go to the reviews tab"""
 
 #    return get_element_from_carousel('Отзывы')
@@ -254,17 +247,18 @@ def get_business_menu():
 """
 
 
-#main_page = get_element_from_carousel('Обзор')
-#main_page.click()
-#get_business_menu()
+# main_page = get_element_from_carousel('Обзор')
+# main_page.click()
+# get_business_menu()
 
-#time.sleep(10)
+# time.sleep(10)
 # window = driver.current_window_handle
 # driver.switch_to_window(window)
 # driver.close()
 
 
 # get_business_menu()
+
 browser = Browser()
 browser.driver.get(
     url="https://yandex.ru/maps/193/voronezh/?ll=39.198713%2C51.633190&z=16.06"
@@ -272,21 +266,119 @@ browser.driver.get(
 test_write_plus_it(browser)
 browser.company_found = True
 
-# work with photo
-photo_obj = YandexPhoto(browser)
-photo_obj.go_to_photo()
-photo_obj.scroll_content()
 
-# work with review
-review_obj = YandexReviews(browser)
-review_obj.go_to_review()
-review_obj.scroll_content()
+def photo_func():
+    """work with photo"""
 
-# work with auth
-auth_obj = YandexAuth(browser)
-auth_obj.auth(
-    login='y4ndex.genag4448@yandex.ru',
-    password='Kkq-MUv-rSw-3zv-!@#'
-)
+    photo_obj = YandexPhoto(browser)
+
+    # get tab or error of click
+    errors = photo_obj.go_to_photo().get('errors')
+    if errors != 0:
+        return {
+            'error': 1
+        }
+
+    photo_obj.scroll_content()
+
+    return {
+        'error': 0
+    }
+
+
+def review_func():
+    """work with review"""
+
+    review_obj = YandexReviews(browser)
+
+    errors = review_obj.go_to_review().get('errors')
+    if errors != 0:
+        return {
+            'error': 1
+        }
+
+    review_obj.scroll_content()
+
+    return {
+        'error': 0
+    }
+
+
+def auth_func():
+    """work with auth"""
+    auth_obj = YandexAuth(browser)
+    auth_obj.auth(
+        login='y4ndex.genag4448@yandex.ru',
+        password='Kkq-MUv-rSw-3zv-!@#',
+        return_func=test_write_plus_it
+    )
+
+    return {
+        'error': 0
+    }
+
+
+data_set = [
+    {
+        'name': 'photo',
+        'func': photo_func,
+        'link': True
+    },
+    {
+        'name': 'reviews',
+        'func': review_func,
+        'link': True
+    },
+    {
+        'name': 'auth',
+        'func': auth_func,
+        'link': False
+    }
+]
+error_data_set = []
+
+logic = True
+first = 0
+while logic:
+    # get random elem
+    if first == 0:
+        rand_int = 2
+        first = 1
+    else:
+        rand_int = random.randint(0, len(data_set) - 1)
+
+    # control on error
+    if data_set[rand_int].get('func')().get('error') == 1:
+        error_data_set.append(
+            data_set.pop(rand_int)
+        )
+    else:
+        if data_set[rand_int].get('link'):
+            data_set.pop(rand_int)
+
+            # work with linked elements
+            while_pass = True
+            while while_pass:
+                for_while_pass = 0
+                for i in range(len(data_set)):
+                    if data_set[i].get('link'):
+                        for_while_pass += 1
+                        data_set.pop(i).get('func')()
+                        break
+
+                if for_while_pass == 0:
+                    while_pass = False
+
+            # work with error's elements
+            for i in error_data_set:
+                i.get('func')()
+
+        else:
+            data_set.pop(rand_int).get('number')
+
+    # control length of a_a
+    if len(data_set) == 0:
+        logic = False
+
 
 input()
